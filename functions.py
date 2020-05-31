9adbd74b6a20421b363cee2b5212abbd8fb363d8
ongkuayhock <66134238+ongkuayhock@users.noreply.github.com>
2020-05-31 19:32:24 +0800

    Functions in python

---
+++ functions.py
@@ -0,0 +1,11 @@
+## Python functions devleoped
+
+def square (x):
+    return ( x * x)
+
+def main():
+    for i in range(5):
+        print(f"Square of {i} is {square(i)}")
+
+if __name__ == "__main__":
+    main()
